1a5124457d16c9f66f9d3a408d207f03c10700af
unknown <madjas00@gmail.com>
2018-07-23 17:39:01 -0500

    set up book objects

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,30 @@
 
 var app = app || {};
 
-const ENV = {
-    isProduction:/^(?!localhost|127)/.test(window.location.hostname),
-    productionApiUrl:'https://tr-et-booklist.herokuapp.com',
-    devApiUrl:'http://localhost:3000',
-    ENVIRONMENT = {
-        apiUrl: isProduction ? productionApiUrl: developmentApiUrl
+(function(module){
+    module.ENV = {
+        isProduction:/^(?!localhost|127)/.test(window.location.hostname),
+        productionApiUrl:'https://tr-et-booklist.herokuapp.com',
+        devApiUrl:'http://localhost:3000',
+        ENVIRONMENT = {
+            apiUrl: isProduction ? productionApiUrl: developmentApiUrl
+        }
     }
-}
+    module.showOnly = function(selected) {
+        $("main > section").hide()
+        $(selected).show()
+    }
+    let templateCache = {};
+    module.render = function(templateId, dataToRender) {
+        let template = templateCache[templateId];
+
+        if(!template) {
+            template = Handlebars.compile(document.getElementById(templateId).innerText);
+
+            templateCache[templateId] = template;
+        }
+        return template(dataToRender);
+    }
+
+  
+})(app);
