3aef74951d94fb874e03a20bd334f0640f7f1708
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-07-24 16:32:15 -0500

    fixed load

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -2,28 +2,30 @@
 var app = app || {};
 
 (function(module){
-        module.Book = function (title, author, isbn, image_url, description) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.image_url = image_url;
-        this.description = description;
+    let Book = function (book) {
+        this.title = book.title;
+        this.author = book.author;
+        this.isbn = book.isbn;
+        this.image_url = book.image_url;
+        this.description = book.description;
     }
-    Book.toHtml = function() {
+    Book.prototype.toHtml = function() {
         return app.render('bookTemplate', this);
     }
 
     Book.all = [];
     const compareBy = (key) => (a, b) => a[key] < b[key] ? -1 : a[key] > b[key] ? 1 : 0;
     Book.loadAll = function(rows){
+        console.log(rows);
         Book.all = rows.sort(compareBy('title')).map(book => new Book(book));
+        console.log(Book.all)
     }
 
-    Book.fetchAll = callback =>
-    $.get(`${app.ENV.ENVIRONMENT.apiUrl}/books`)
+    Book.fetchAll = (callback,errorCallback) =>
+    $.get(`${app.ENV.apiUrl}/api/v1/books`)
       .then(Book.loadAll)
       .then(callback)
       .catch(errorCallback);
 
-  module.Book = Book;
+    module.Book = Book
 })(app);
