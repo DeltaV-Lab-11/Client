10f1752ed382702c59b7175c1954c7a5a48bab69
Taylor Robinson <madjas00@gmail.com>
2018-07-25 18:06:31 -0500

    worked on updates

--- scripts/controller/routes.js
+++ scripts/controller/routes.js
@@ -5,5 +5,8 @@ page('/books/new', ()=> app.bookView.initAddForm());
 page('/books/:id', ctx=> {
     app.Book.fetchOne(ctx.params.id,app.bookView.initDetailPage);
 });
+page('/books/:id/update', ctx=> {
+    app.Book.fetchOne(ctx.params.id, app.bookView.initUpdatePage);
+});
 
 page();
